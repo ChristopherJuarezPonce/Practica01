a7759fc5e16c35e0dbd546fc5457773a20d90843
Christopher Juarez <00127525@uca.edu.sv>
2026-09-17 19:15:10 -0600

    feat: agregar estructura de paneles solares
    Se agrega la estructura struct para representar los paneles solares.

--- main.cc
+++ main.cc
@@ -1,4 +1,19 @@
 #include <iostream>
+#include <string>
+
+/*Un grupo de ingenieros necesita auditar el consumo eléctrico de varios paneles 
+solares en una planta fotovoltaica. El sistema trabajará con un máximo de 10 paneles.*/
+
+// Crear la estructura para el panel solar
+struct PanelSolar {
+    int codigo;
+    std::string ubicacion;
+    float potenciaTeorica; //Potencia maxima ideal de watts
+    float lecturas [3]; //Tres mediciones de potencia real
+    float eficiencia; //Porcentaje de rendimiento real vs teorico
+    std::string estadoOperativo; //Clasificacion segun eficiencia "Deficiente", "Aceptable", "Optimo" y "Anomalia"
+};
+
 
 int main(){
     return 0;
