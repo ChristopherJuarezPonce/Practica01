d64e18227d88ae8a7da71b19c8908c413043bfc7
Christopher Juarez <00127525@uca.edu.sv>
2026-09-17 21:39:40 -0600

    feat: implementar registro de paneles solares
    Se agrega registrarPanel para permitir al usuario registrar sus paneles desde main, con una validación que limita el registro a un máximo de 10 paneles.

--- main.cc
+++ main.cc
@@ -14,7 +14,52 @@ struct PanelSolar {
     std::string estadoOperativo; //Clasificacion segun eficiencia "Deficiente", "Aceptable", "Optimo" y "Anomalia"
 };
 
+//Declaracion de todas las funciones para el programa
+void registrarPanel(PanelSolar &panel);
+
 
 int main(){
+
+    //Declaracion de maximo 10 paneles solares
+    PanelSolar paneles[10];
+    int n; // Variable para almacenar el numero de paneles
+
+    /*Este do while sirve para validar que el numero de paneles ingresado
+    sea entre 1 y 10, si no es asi se le pedira al usuario que ingrese un 
+    numero valido*/
+    do
+    {
+        std::cout << "Ingrese el numero de paneles (maximo 10): ";
+        std::cin >> n;
+        if(n < 1 || n > 10);
+        {
+            std::cout << "El numero de elementos es invalido. Debe ser entre el 1 y 10." << std::endl;
+        }
+    } while (n < 1 || n > 10 );
+
+    // Parte 1: Registrar los paneles solares
+    for (int i = 0; i < n; i++) // Este for sirve para recorrer el arreglo de paneles solares y registrar cada uno de ellos
+    {
+        std::cout << "Panel " << i + 1 << ":" << std::endl;
+        registrarPanel(paneles[i]);
+    }
+    
+
     return 0;
+}
+
+//Registrar los paneles solares
+void registrarPanel(PanelSolar &panel){
+    std::cout << "Ingrese el codigo del elemento: ";
+    std::cin >> panel.codigo;
+    std::cout << "Ingrese la ubicacion del panel: ";
+    std::cin >> panel.ubicacion;
+    std::cout << "Ingrese la potencia teorica: ";
+    std::cin >> panel.potenciaTeorica;
+    std::cout << "Ingrese la lectura 1: ";
+    std::cin >> panel.lecturas[0];
+    std::cout << "Ingrese la lectura 2: ";
+    std::cin >> panel.lecturas[1];
+    std::cout << "Ingrese la lectura 3: ";
+    std::cin >> panel.lecturas[2];
 }
